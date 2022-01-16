0ebbcfda2af09c1a4d99395fb4cfce96b161b6a1
Lucas Oliveira <87589466+LuskarDev@users.noreply.github.com>
2022-01-16 18:59:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const chk = document.getElementById('chk');
+const circle = document.querySelector('.circle')
 
 chk.addEventListener('change', () => {
+	circle.classList.toggle('ative')
 	document.body.classList.toggle('dark');
 });
